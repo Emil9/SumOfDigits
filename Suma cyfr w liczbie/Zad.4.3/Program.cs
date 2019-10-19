acd29b51a58be23831cd07f68b1f82f9900b8c76
Emil Grabowski <43807341+Emil9@users.noreply.github.com>
2019-10-19 17:10:34 +0200

    Add files via upload

---
+++ Suma cyfr w liczbie/Zad.4.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad._4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 1, suma = 0;
+            Console.Write("Podaj liczbę n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (i <= n)
+            {
+                suma += n % 10;
+                n /= 10;
+            }
+            Console.Write("Suma cyfr tej liczby wynosi {0}", suma);
+            Console.ReadKey(true);
+        }
+    }
+}
